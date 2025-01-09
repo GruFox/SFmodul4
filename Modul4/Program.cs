5f6241b9c14d06e689e69a96152731ebfe34975f
Gru <energimen@yandex.ru>
2025-01-09 11:41:43 +0100

    Добавьте файлы проекта.

---
+++ Modul4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+
+namespace Modul4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string A;
+            string B;
+            bool C = A != B;*/
+
+            /*int A;
+            int B;
+            double X;
+            double Y;
+            bool C = (A < B) | (X > Y);*/
+
+            Console.Write("Напишите свой любимый цвет на английском с маленькой буквы: ");
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+
+            
+
+        }
+    }
+}
