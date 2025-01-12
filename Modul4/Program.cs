9a1f6f27c2c50bd528a4b1293f0a69a327a4283a
Gru <energimen@yandex.ru>
2025-01-12 19:57:09 +0100

    Массивы

--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.Design;
-
-namespace Modul4
+﻿namespace Modul4
 {
     internal class Program
     {
@@ -10,13 +8,17 @@ namespace Modul4
             string B;
             bool C = A != B;*/
 
+
+
             /*int A;
             int B;
             double X;
             double Y;
             bool C = (A < B) | (X > Y);*/
 
-            int t = 0;
+
+
+            /*int t = 0;
 
             while (true)
             {
@@ -63,10 +65,191 @@ namespace Modul4
                         continue;
                 }
                 t++;
-            }
-            
+            }*/
 
-            
+
+
+            /*Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ваше имя по буквам:");
+            foreach (var wordsInName in name)
+            {
+                Console.Write(wordsInName + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Последняя буква вашего имени: " + name[name.Length - 1]);
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }*/
+
+
+
+
+            /*int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+            for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+            {
+                for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+                {
+                    Console.Write(array[k, i] + " ");
+                }
+
+                Console.WriteLine();
+            }*/
+
+
+
+
+            /*var arr = new[] { 5, 6, 9, 1, 2, 3, 4 };
+            int[] arr2 = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length + 1; i++)
+            {
+                int b = arr[i];
+
+                foreach (var number in arr2)
+                {
+                    if (number > arr2[i])
+                    {
+                        continue;
+                    } else
+                    {
+                        arr2[i] = b;
+                    }
+                }
+
+            }
+            Console.Write(arr2);*/
+
+
+
+
+            /*Console.WriteLine("Введите 3 своих любимых цвета:");
+            string[,] favcolors = new string[,] { { "color1" }, { "color2" }, { "color3" } };
+            var t = 0;
+            var n = 1;
+            while (true)
+            {
+                if (n > 3)
+                {
+                    Console.WriteLine("Вы ввели требуемое количество цветов!");
+                    break;
+                }
+                Console.WriteLine("Введите свой любимый цвет номер " + n);
+                string inter = Console.ReadLine();
+                if (inter == "stop")
+                {
+                    Console.WriteLine("Цикл остановлен!");
+                    break;
+                }   else
+                {
+                    favcolors[t, 0] = inter;
+                }
+
+                t++;
+                n++;
+            }
+
+            foreach (var color in favcolors)
+            {
+                foreach (var item in color)
+                {
+                    Console.Write(item + " ");
+                }
+            }*/
+
+
+
+
+            /*int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine("Четные числа массива: ");
+
+            foreach (int p in arr)
+            {
+                int ost = p % 2;
+                if (ost == 0) Console.Write(p + " ");
+
+                //else continue;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Перебор окончен");*/
+
+
+
+
+
+            /*int[] array = { -10, 9, -8, 7, -6, 5, -4, 3, -2, 1, 0 };
+            int numbers = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                    numbers++;
+            }
+
+            Console.WriteLine(numbers);*/
+
+
+
+
+            /*int[][] array = [ [ -10, 9, -8, 7, -6, 5 ], [ -4, 3, -2, 1, 0, -11 ] ];
+            int numbers = 0;
+            foreach (var i in array)
+            {
+                foreach (int k in i)
+                {
+                    if (k > 0) numbers++;
+                }
+            }
+            Console.WriteLine(numbers);*/
+
+
+
+
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int k = 0; k < arr.GetLength(1); k++)
+                {
+                    Console.Write(arr[i, k] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int k = 0; k < arr.GetLength(1) - 1; k++)
+                {
+                    if (arr[i, k] > arr[i, k + 1])
+                    {
+                        int first = arr[i, k];
+                        arr[i, k] = arr[i, k + 1];
+                        arr[i, k + 1] = first;
+
+                        k = -1;
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int k = 0; k < arr.GetLength(1); k++)
+                {
+                    Console.Write(arr[i, k] + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+
 
         }
     }
